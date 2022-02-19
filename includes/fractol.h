85ec0b61d7e38025b286eb8ff4cbed5b99cb2c03
Sergio10G <sejobmx@hotmail.es>
2022-02-19 18:44:09 +0100

    header file updated

--- includes/fractol.h
+++ includes/fractol.h
@@ -6,7 +6,7 @@
 /*   By: sdiez-ga <sdiez-ga@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/10 17:39:57 by sdiez-ga          #+#    #+#             */
-/*   Updated: 2022/02/11 18:01:47 by sdiez-ga         ###   ########.fr       */
+/*   Updated: 2022/02/18 19:02:56 by sdiez-ga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,29 +15,19 @@
 
 //	Libraries
 # include "../minilibx/mlx.h"
+# include "../libft/libft.h"
 # include <math.h>
 # include <unistd.h>
 # include <fcntl.h>
 # include <stdlib.h>
 # include <stdio.h>
+# include <string.h>
+
+//	Macros
+# define DEF_WIDTH 1920
+# define DEF_HEIGHT 1080
 
 // Structs
-
-typedef struct s_metadata
-{
-	int		width;
-	int		height;
-	double	scale;
-	double	offset_x;
-	double	offset_y;
-}				t_metadata;
-
-typedef struct	s_mlxvars
-{
-	void		*mlx;
-	void		*win;
-	t_metadata	*md;
-}				t_mlxvars;
 
 typedef struct	s_imgdata 
 {
@@ -54,15 +44,49 @@ typedef struct	s_complex
 	double im;
 }				t_complex;
 
+typedef struct	s_fractaldata
+{
+	unsigned int	(*fractal)(t_complex*, t_complex*, unsigned int, unsigned int);
+	unsigned int	iters;
+	t_list			**colors;
+	int				color_count;
+}				t_fractaldata;
+
+typedef struct	s_vars
+{
+	void			*mlx;
+	void			*win;
+	int				width;
+	int				height;
+	double			scale;
+	double			offset_x;
+	double			offset_y;
+	t_imgdata		*id;
+	t_fractaldata	*fd;
+}				t_vars;
 //	Functions
 
-/* metadata_funcs.c functions */
-t_metadata *init_metadata(int width, int height);
+/* fractol.c functions */
+void	print_image(t_vars *vars);
 
-/* complex_funcs.c functions */
-t_complex	*init_complex();
+/* input_parse.c */
+void	check_params(int argc, char **argv);
+void	check_julia_params(char *params_cli);
+
+/* utils.c functions */
+void	ft_strtoupper(char *str);
+
+/* init_structss.c functions */
+t_complex		*init_complex();
+t_vars			*init_vars();
+t_fractaldata	*init_fractaldata();
 
 /* algorithm.c functions */
 unsigned int	mandelbrot(t_complex z, t_complex c, unsigned int iters, unsigned int i);
+unsigned int	julia(t_complex z, t_complex c, unsigned int iters, unsigned int i);
+unsigned int	burning_ship(t_complex z, t_complex c, unsigned int iters, unsigned int i);
+
+/* errors.c functions */
+void	err_print_options(int errnum);
 
 #endif
