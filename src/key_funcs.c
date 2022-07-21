77b64fe0a691905d29b4adbd7716d22795f8da56
Sergio10G <sejobmx@hotmail.es>
2022-07-21 15:30:17 +0200

    linux branch updated

--- src/key_funcs.c
+++ src/key_funcs.c
@@ -6,22 +6,20 @@
 /*   By: sdiez-ga <sdiez-ga@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/11 17:42:46 by sdiez-ga          #+#    #+#             */
-/*   Updated: 2022/02/25 18:55:36 by sdiez-ga         ###   ########.fr       */
+/*   Updated: 2022/07/21 15:21:08 by sergiodg         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/fractol.h"
 
-int	key_hook(int keycode, void *param)
+int	key_hook(int keycode, t_vars *vars)
 {
-	t_vars	*vars;
 	// Up: 126
 	// Down: 125
 	// Left: 123
 	// Right: 124
 	// Esc: 53
-	vars = (t_vars *)param;
-	//printf("keycode: %d\n", keycode);
+	printf("keycode: %d\n", keycode);
 	change_colors(keycode, vars);
 	if (keycode == 65307)
 	{
@@ -36,23 +34,25 @@ int	key_hook(int keycode, void *param)
 		vars->offset_x += 0.1;
 	else if (keycode == 65363)
 		vars->offset_x -= 0.1;
-	vars->fd->paint_fractal(vars);
+	else if (keycode == 65451)
+		vars->scale += vars->scale / 10.0;
+	else if (keycode == 65453)
+		vars->scale -= vars->scale / 10.0;
+	paint_fractal(vars);
 	return (0);
 }
 
-int	mouse_hook(int button, int x, int y, void *param)
+int	mouse_hook(int button, int x, int y, t_vars *vars)
 {
-	t_vars	*vars;
 	// ScrollUp: 4
 	// ScrollDown: 5
 	(void) x;
 	(void) y;
-	vars = (t_vars *)param;
 	if (button == 4)
 		vars->scale += vars->scale / 10.0;
 	else if (button == 5)
 		vars->scale -= vars->scale / 10.0;
-	vars->fd->paint_fractal(vars);
+	paint_fractal(vars);
 	return (0);
 }
 
